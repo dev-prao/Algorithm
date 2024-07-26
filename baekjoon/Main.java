f5e32f18d1bb7116c85cb4170e1cbe80733c2ddf
prao <pch5521@gmail.com>
2024-07-26 23:41:59 +0900

    "add"

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -3,100 +3,79 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
 
+	static int N, M;
+	static int total = 0;
+	static int index = 1;
+
+	static boolean[] canEscape;
+	static int[][] maze, visited;
+
+	static int[] dr = {-1, 0, 1, 0};
+	static int[] dc = {0, 1, 0, -1};
+	static HashMap<Character, Integer> directions = new HashMap<>();
+
 	public static void main(String[] args) throws IOException {
+
+		directions.put('U', 0);
+		directions.put('R', 1);
+		directions.put('D', 2);
+		directions.put('L', 3);
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		int n = Integer.parseInt(br.readLine());
-
-		int[] A = new int[n];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		A[0] = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-		for (int i = 1; i < n; i++) {
-			A[i] = Integer.parseInt(st.nextToken()) + A[i - 1];
-		}
+		maze = new int[N][M];
+		visited = new int[N][M];
 
+		canEscape = new boolean[N * M + 1];
 
-
-		int m = Integer.parseInt(br.readLine());
-
-		int[] B = new int[m];
-		st = new StringTokenizer(br.readLine());
-		B[0] = Integer.parseInt(st.nextToken());
-
-		for (int i = 1; i < m; i++) {
-			B[i] = Integer.parseInt(st.nextToken()) + B[i - 1];
-		}
-
-		int lenA = (n * (n + 1)) / 2;
-		int lenB = (m * (m + 1)) / 2;
-
-		int[] sumA = new int[lenA];
-
-		int idx = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = i; j < n; j++) {
-				if(i == 0) {
-					sumA[idx++] = A[j];
-					continue;
-				}
-				sumA[idx++] = A[j] - A[i - 1];
+		for (int i = 0; i < N; i++) {
+			String input = br.readLine();
+			for (int j = 0; j < M; j++) {
+				char c = input.charAt(j);
+				maze[i][j] = directions.get(c);
 			}
 		}
 
-		int[] sumB = new int[lenB];
-
-		idx = 0;
-		for (int i = 0; i < m; i++) {
-			for (int j = i; j < m; j++) {
-				if(i == 0) {
-					sumB[idx++] = B[j];
-					continue;
-				}
-				sumB[idx++] = B[j] - B[i - 1];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (visited[i][j] != 0) continue;
+				dfs(i, j, 0);
+				index++;
 			}
 		}
 
-		Arrays.sort(sumA);
-		Arrays.sort(sumB);
+		System.out.println(total);
+		br.close();
+	}
 
-		int res = 0;
-		int left = 0;
-		int right = lenB - 1;
-
-		while (left < lenA && right >= 0) {
-			int curA = sumA[left];
-			int curB = sumB[right];
-			int sum = curA + curB;
-			if (sum == T) {
-				int cntA = 0;
-				int cntB = 0;
-				while (left < lenA && sumA[left] == curA) {
-					cntA++;
-					left++;
-				}
-
-				while (right >= 0 && sumB[right] == curB) {
-					cntB++;
-					right--;
-				}
-
-				res += cntA * cntB;
-				continue;
-			}
-
-			if (sum < T) {
-				left++;
-				continue;
-			}
-
-			right--;
+	public static void dfs(int r, int c, int count) {
+		if (isOutOfMap(r, c)) {
+			canEscape[index] = true;
+			total += count;
+			return;
 		}
-		System.out.println(res);
+
+		if (visited[r][c] != 0 && canEscape[visited[r][c]]) {
+			canEscape[index] = true;
+
+			total += count;
+		}
+
+		if(visited[r][c] != 0) return;
+
+		visited[r][c] = index;
+		dfs(r + dr[maze[r][c]], c + dc[maze[r][c]], count + 1);
+	}
+
+	public static boolean isOutOfMap(int r, int c) {
+		return r < 0 || r >= N || c < 0 || c >= M;
 	}
 }
